c7bf262f1fc29844d5b625f3d4a940a7fe219e69
ankittailor10 <ankit.tailor@isimplexity.com>
2021-08-15 03:03:35 +0530

    feat: change bg color after submit

--- quiz.js
+++ quiz.js
@@ -2,6 +2,7 @@ const rightAnwsers = ["option1", "option1", "option1", "option3", "option2"]
 
 const formQuiz = document.forms[0];
 const showScore = document.querySelector('.score')
+const questionDiv = document.querySelectorAll('.radio')
 
 formQuiz.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -9,7 +10,10 @@ formQuiz.addEventListener('submit', function (e) {
     let index = 0, score = 0;
     for (let entry of data) {
         if (entry[1] == rightAnwsers[index]) {
+            questionDiv[index].style.backgroundColor = "lightgreen";
             score++;
+        } else {
+            questionDiv[index].style.backgroundColor = "pink";
         }
         index++;
     }
